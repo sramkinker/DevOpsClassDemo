7e682c06a161b69224ae2ef0cc2642e025c96ab9
sramkinker <sramkinker@gmail.com>
2019-07-27 07:27:06 +0530

    Update GenericComparator.java

--- src/main/java/com/srkdemo/utilities/GenericComparator.java
+++ src/main/java/com/srkdemo/utilities/GenericComparator.java
@@ -1,4 +1,4 @@
-package com.edurekademo.utilities;
+package com.srkdemo.utilities;
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
